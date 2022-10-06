ccfcc45d62a4bf966222ef0388d7f71342f391cf
zak aden <s.aden159@gmail.com>
2022-10-06 19:11:59 +0100

    create nav component

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Navbar from '../../components/Navbar'
 
 import "./home.scss"
 
 const Home = () => {
   return (
-    <div>
+    <div className='home'>
       home
+      <Navbar />
     </div>
   )
 }
