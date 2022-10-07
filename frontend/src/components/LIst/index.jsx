c94cfc1290491b0399a9cd167471b691adf2a4a9
zak aden <s.aden159@gmail.com>
2022-10-07 20:15:42 +0100

    add ability to go left or right with slider

--- frontend/src/components/LIst/index.jsx
+++ frontend/src/components/LIst/index.jsx
@@ -3,15 +3,28 @@ import { ArrowBackIosNewOutlined, ArrowForwardIosOutlined } from '@mui/icons-mat
 
 import './styles.scss'
 import ListCard from '../ListCard'
+import { useRef } from 'react'
 
 const List = () => {
   const abc = [1, 2, 3, 4, 5, ,6 ,7 ,8, 9, 0, 2, 34, 4, 4, 4, 3, 4,5 ]
+
+  const listRef = useRef()
+
+  const handleClick = (direction) => {
+    let distance = listRef.current.getBoundingClientRect().x - 50
+    if(direction === 'left') {
+      listRef.current.style.transform = `translateX(${distance + 230}px)`
+    }
+    if(direction === 'right') {
+      listRef.current.style.transform = `translateX(${distance - 230}px)`
+    }
+  }
   return (
     <div className='list'>
       <h2 className='title'>Continue to watch</h2>
       <div className='wrapper'>
-        <ArrowBackIosNewOutlined className='slider left'/>
-        <div className='container'>
+        <ArrowBackIosNewOutlined className='slider left' onClick={() => handleClick('left')}/>
+        <div className='container' ref={listRef}>
           {abc.map(() => (
             <ListCard/>
           ))}
@@ -30,7 +43,7 @@ const List = () => {
           <ListCard/>
           <ListCard/>
         </div>
-        <ArrowForwardIosOutlined className='slider right'/>
+        <ArrowForwardIosOutlined className='slider right'  onClick={() => handleClick('right')}/>
       </div>
     </div>
   )
