fba7d30e9988068252ec3d2d13ddfeeeac93c8c3
zak aden <s.aden159@gmail.com>
2022-10-07 21:33:18 +0100

    styling fix for list component

--- frontend/src/components/LIst/index.jsx
+++ frontend/src/components/LIst/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ArrowBackIosNewOutlined, ArrowForwardIosOutlined } from '@mui/icons-material'
 
 import './styles.scss'
@@ -6,11 +6,13 @@ import ListCard from '../ListCard'
 import { useRef } from 'react'
 
 const List = () => {
+  const [isMoved, setIsMoved] = useState(false)
   const abc = [1, 2, 3, 4, 5, ,6 ,7 ,8, 9, 0, 2, 34, 4, 4, 4, 3, 4,5 ]
 
   const listRef = useRef()
 
   const handleClick = (direction) => {
+    setIsMoved(true)
     let distance = listRef.current.getBoundingClientRect().x - 50
     if(direction === 'left') {
       listRef.current.style.transform = `translateX(${distance + 230}px)`
@@ -23,7 +25,7 @@ const List = () => {
     <div className='list'>
       <h2 className='title'>Continue to watch</h2>
       <div className='wrapper'>
-        <ArrowBackIosNewOutlined className='slider left' onClick={() => handleClick('left')}/>
+        <ArrowBackIosNewOutlined style={{display: !isMoved && 'none'}} className='slider left' onClick={() => handleClick('left')}/>
         <div className='container' ref={listRef}>
           {abc.map(() => (
             <ListCard/>
