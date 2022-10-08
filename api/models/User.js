c98d8dc786c4f33dd3bccce08f645e43145c7313
zak aden <s.aden159@gmail.com>
2022-10-09 00:27:38 +0100

    create model schema for movie

--- api/models/User.js
+++ api/models/User.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
-const userSchema = new Schema({
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
   email: {
     type: String,
     required: true,
@@ -10,7 +15,15 @@ const userSchema = new Schema({
   password: {
     type: String,
     required: true
+  },
+  profilePic: {
+    type: String,
+    default: ''
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
   }
-})
+}, {timestamps: true})
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', UserSchema);
